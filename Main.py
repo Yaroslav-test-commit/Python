a6d6387e0d89b95cb3585a81dbfd2cd3fc8518f9
Your Name <yaroslavqaeng@gmail.com>
2025-04-22 14:57:37 +0300

    Исправил код для 'Простой калькулятор'

--- Main.py
+++ Main.py
@@ -1,41 +1,27 @@
 try:
     num1 = float(input("Введите первое число: "))
     num2 = float(input("Введите второе число: "))
+    operation =  input("Введите операцию (+, -, *, /): ")
+    number = num1.is_integer() and num2.is_integer()
 
-    if num1.is_integer() and num2.is_integer():
-        print(f"Результат сложения: {int(num1 + num2)}")
-    else: print(f"Результат сложения: {num1 + num2}")
-
-except ValueError:
-    print("Ошибка: введено не число!")
-
-try:
-    num1 = float(input("Введите первое число: "))
-    num2 = float(input("Введите второе число: "))
-
-    if num1.is_integer() and num2.is_integer():
-        print(f"Результат вычитания: {int(num1 - num2)}")
-    else: print(f"Результат вычитания: {num1 - num2}")
-
-except ValueError:
-    print("Ошибка: введено не число!")
-
-try:
-    num1 = float(input("Введите первое число: "))
-    num2 = float(input("Введите второе число: "))
-
-    if num1.is_integer() and num2.is_integer():
-        print(f"Результат умножения: {int(num1 * num2)}")
-    else: print(f"Результат умножения: {num1 * num2}")
-
-except ValueError:
-    print("Ошибка: введено не число!")
-
-try:
-    num1 = float(input("Введите первое число: "))
-    num2 = float(input("Введите второе число: "))
-
-    print(f"Результат деления: {num1 / num2}")
+    if operation == "+":
+        if number:
+            print(f"Результат сложения: {int(num1 + num2)}")
+        else: print(f"Результат сложения: {num1 + num2}")
+    elif operation == "-":
+        if number:
+            print(f"Результат вычитания: {int(num1 - num2)}")
+        else: print(f"Результат вычитания: {num1 - num2}")
+    elif operation == "*":
+        if number:
+            print(f"Результат умножения: {int(num1 * num2)}")
+        else: print(f"Результат умножения: {num1 * num2}")
+    elif operation == "/":
+        if (num1 / num2).is_integer:
+            print(f"Результат деления: {int(num1 / num2)}")
+        else: print(f"Результат деления: {num1 / num2}")
+    else:
+        print("Ошибка: неизвестная операция")
 
 except ValueError:
     print("Ошибка: введено не число!")
