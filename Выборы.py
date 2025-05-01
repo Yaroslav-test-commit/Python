776ab6b7270cf2513d172e53aa747717bd473826
Your Name <yaroslavqaeng@gmail.com>
2025-05-01 14:32:30 +0300

    Исправил программу проверки допуска к голосованию.

--- Выборы.py
+++ Выборы.py
@@ -1,6 +1,7 @@
 age = int(input("Введите Ваш возраст: "))
 citizenship = input("Вы являетесь гражданином страны? (да/нет): ").strip().lower() == "да"
 disqualified = input("Вы привлекались к уголовной/административной ответственности? (да/нет): ").strip().lower() == "да"
-admission = "Вы имеете право принять участие в голосовании" if age >= 18 and citizenship and not disqualified else "Вы пока не можете принять участие в голосовании"
-
-print(admission)
+if age >= 18 and citizenship and not disqualified:
+    print("Вы имеете право принять участие в голосовании")
+else:
+    print("Вы пока не можете принять участие в голосовании")
