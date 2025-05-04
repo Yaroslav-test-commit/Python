a2393c32f13625071558e09cfbcf2389db32d317
Your Name <yaroslavqaeng@gmail.com>
2025-05-04 20:17:21 +0300

    Исправил подсчет четных чисел в промежутке (1, 100) включительно через функцию sum.

--- Применение условий.py
+++ Применение условий.py
@@ -1,12 +1,4 @@
-total_sum = 0 # сумма всех четных чисел от 1 до 100
-for i in range(1, 100):
-    if i % 2 == 0:
-        total_sum += i
-
-total_sum_new = 0 # сумма всех четных чисел от 1 до 100 включительно
-for i in range(1, 101):
-    if i % 2 == 0:
-        total_sum_new += i
+total_sum = sum(i for i in range(1, 101) if i % 2 == 0)
 
 squares = [n ** 2 for n in range(0, 10) if n % 2 != 0]
 
@@ -15,6 +7,5 @@ while (number := int(input("Введите число: ")) > 0):
     counter += 1
 
 print(total_sum)
-print(total_sum_new)
 print(squares)
 print(counter)
