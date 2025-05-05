f7d6feed901f03f2a3bbd8f661a6d9f72ba2d734
Your Name <yaroslavqaeng@gmail.com>
2025-05-05 15:31:22 +0300

    Исправил программу сложения элементов двух списков.

--- Сложение элементов.py
+++ Сложение элементов.py
@@ -1,5 +1,10 @@
-my_list = [1, 2, 3, 4, 5]
-my_list_new = [1, 2, 3, 4, 5]
-list_new = [i + i for i in my_list and my_list_new]
+my_list = [1, 2, 3, 4]
+my_list_new = [1, 2, 3, 4, 5, 6]
+max_len = max(len(my_list), len(my_list_new))
+my_list_modified = my_list + [0] * (max_len - len(my_list))
+my_list_new_modified = my_list_new + [0] * (max_len - len(my_list_new))
+list_new = []
+for i in range(max_len):
+    list_new.append(my_list_modified[i] + my_list_modified[i])
 
 print(list_new)
