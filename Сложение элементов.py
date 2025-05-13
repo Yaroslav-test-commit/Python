e40208a7ba55a3de4f2b6513f854a6145dc66c9d
Your Name <yaroslavqaeng@gmail.com>
2025-05-13 13:40:58 +0300

    Исправил программу сложения элементов двух списков.

--- Сложение элементов.py
+++ Сложение элементов.py
@@ -1,10 +1,25 @@
-my_list = [1, 2, 3, 4]
-my_list_new = [1, 2, 3, 4, 5, 6]
-max_len = max(len(my_list), len(my_list_new))
-my_list_modified = my_list + [0] * (max_len - len(my_list))
-my_list_new_modified = my_list_new + [0] * (max_len - len(my_list_new))
-list_new = []
-for i in range(max_len):
-    list_new.append(my_list_modified[i] + my_list_modified[i])
+def sum_list(list_1, list_2):
+    list_new = []
+    if len(list_1) != len(list_2):
+        if len(list_1) > len(list_2):
+            diff = len(list_1) - len(list_2)
+            for i, v in enumerate(list_1):
+                if i >= len(list_1) - diff:
+                    list_new.append(v)
+                else:
+                    list_new.append(v + list_2[i])
+        else:
+            diff = len(list_2) - len(list_1)
+            for i, v in enumerate(list_2):
+                if i >= len(list_2) - diff:
+                    list_new.append(v)
+                else:
+                    list_new.append(v + list_1[i])
+    else:
+        for i, v in enumerate(list_1):
+            list_new.append(v + list_2[i])
+    return list_new
 
-print(list_new)
+
+result = sum_list([1, 2, 3, 5, 7], [4, 5, 6, 7])
+print(result)
